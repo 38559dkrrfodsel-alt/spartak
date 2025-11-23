f1645cd06b275e8ec70853c9c9d88d68ee8772a9
38559dkrrfodsel-alt <38559dkrrfodsel@gmail.com>
2025-11-23 21:32:44 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    const scrollPosition = window.scrollY;
+    
+    if (scrollPosition > 100) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
